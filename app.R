a3b1dfb591091f5b9bea7874ed7a6e989cc4ce07
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2019-06-03 16:18:13 +0300

    Adjusted filtering

--- app.R
+++ app.R
@@ -4,17 +4,15 @@ library(leaflet)
 library(mapview)
 library(sf)
 
-# Arvokohteet
-Arvokohteet <- readRDS("trees.RDS")
-# Puut
-Puut <- readRDS("value.RDS")
+Arvokohteet <- readRDS("value.RDS")
+Puut <- readRDS("trees.RDS")
 
 targets <- as.vector(sort(unique(Arvokohteet$kohteen_nimi)))
 
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
-  absolutePanel(bottom = 500, right = 20,
+  absolutePanel(bottom = 400, right = 20,
                 draggable = TRUE,
                 selectInput(inputId = "target",
                             label = "Kohde",
@@ -26,10 +24,12 @@ ui <- bootstrapPage(
 server <- function(input, output, session) {
   
   filteredTargets <- reactive({
-    if(input$target == 'All')
+    if(input$target == 'All') {
       return(Arvokohteet)
-    Arvokohteet %>%
-      filter(kohteen_nimi == input$target)
+    } else {
+      Arvokohteet %>%
+        filter(kohteen_nimi == input$target)
+    }
   })
 
   
@@ -37,28 +37,25 @@ server <- function(input, output, session) {
     
     trees_joined_with_selected_area <- st_join(filteredTargets(), Puut)
     trees_here <- Puut[Puut$id %in% trees_joined_with_selected_area$id.y, ]
-   
-    if(nrow(trees_here) > 0) {
-      
-      m <- mapview(filteredTargets(),
+    
+    area <- mapview(filteredTargets(),
+                 col.regions = "lightskyblue4",
                  popup = leafpop::popupTable(filteredTargets(),
                                              zcol = c("kohteen_nimi",
                                                       "yleiskuvaus",
                                                       "kasvilajistollinen_arvo",
                                                       "kasvillisuus"),
-                                             feature.id = FALSE)) +
-        mapview(trees_here, feature.id = FALSE)
+                                             feature.id = FALSE))
+   
+    if(nrow(trees_here) > 0) {
+      m <- area + mapview(trees_here, 
+                          col.regions = "green",
+                          cex = 5,
+                          feature.id = FALSE)
+    } else {
+      m <- area
     }
-    else {
-      m <- mapview(filteredTargets(),
-                   popup = leafpop::popupTable(filteredTargets(),
-                                               zcol = c("kohteen_nimi",
-                                                        "yleiskuvaus",
-                                                        "kasvilajistollinen_arvo",
-                                                        "kasvillisuus"),
-                                               feature.id = FALSE))
-    }
-    
+   
     m@map
     
   })
