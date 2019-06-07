4cda19899a5137216f92cfab28a6aa0e4f8ed307
Tuija Sonkkila <tuija.sonkkila@aalto.fi>
2019-06-07 11:57:00 +0300

    New attribute to reflect the size of the tree

--- q.R
+++ q.R
@@ -7,6 +7,16 @@ baseurl <- "https://kartta.hel.fi/ws/geoserver/avoindata/wfs?"
 wfs_request <- "request=GetFeature&service=WFS&version=2.0.0&typeName=Puurekisteri_piste&outputFormat=json"
 hki_trees_wfs <- paste0(baseurl,wfs_request)
 trees <- st_read(hki_trees_wfs)
+trees <- trees %>% 
+  mutate(koko = ifelse(kokoluokka == "0 - 10 cm", 0,
+                       ifelse(kokoluokka == "10 - 20 cm", 2,
+                              ifelse(kokoluokka == "20 - 30 cm", 4,
+                                     ifelse(kokoluokka == "30 - 50 cm", 6,
+                                            ifelse(kokoluokka == "50 - 70 cm", 8,
+                                                   ifelse(kokoluokka == "70 cm -", 10, 0)))))))
+
+trees$koko <- ifelse(is.na(trees$kokoluokka), 0, trees$koko)
+
 saveRDS(trees, "trees.RDS")
 
 baseurl <- "https://kartta.hel.fi/ws/geoserver/avoindata/wfs?"
@@ -14,10 +24,4 @@ wfs_request <- "request=GetFeature&service=WFS&version=2.0.0&typeName=Arvoympari
 hki_value_wfs <- paste0(baseurl,wfs_request)
 valueables <- st_read(hki_value_wfs)
 saveRDS(valueables, "value.RDS")
-
-
-
-
-
-
 
